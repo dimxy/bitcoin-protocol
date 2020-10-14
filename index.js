10477a2378debac047d30c0475fb46417e1bd274
dimxy <dimxy@komodoplatform.com>
2020-10-14 16:35:09 +0500

    fixed exports for kmd

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ module.exports = {
   types: require('./src/types'),
   messages: require('./src/messages'),
   constants: require('./src/constants'),
+  kmdtypes: require('./src/kmdtypes'),
+  kmdmessages: require('./src/kmdmessages'),
 
   struct: require('varstruct'),
   varint: require('varuint-bitcoin')
